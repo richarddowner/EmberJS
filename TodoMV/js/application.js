dcbb699eba048b7b868495632438d4d88ab7cb9f
Richard Downer <richard.downer@xero.com>
2014-08-04 08:26:37 +1200

    fixtures

--- TodoMV/js/application.js
+++ TodoMV/js/application.js
@@ -1,1 +1,6 @@
 window.Todos = Ember.Application.create();
+
+// Adapters are responsible for communicating with a source of data for your application.
+// Typically this will be a web service API, 
+// but in this case we are using an adapter designed to load fixture data
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
