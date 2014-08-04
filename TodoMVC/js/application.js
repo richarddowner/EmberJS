1ea5494571bc00179b5251fadab9272b72840f82
Richard <richarddowna@gmail.com>
2014-08-04 16:42:22 +1200

    persistent data using localstorage_adapter.js

--- TodoMVC/js/application.js
+++ TodoMVC/js/application.js
@@ -3,4 +3,9 @@ window.Todos = Ember.Application.create();
 // Adapters are responsible for communicating with a source of data for your application.
 // Typically this will be a web service API, 
 // but in this case we are using an adapter designed to load fixture data
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+// Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// Use persistent adapter
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+	namespace: 'todos-emberjs'
+});
