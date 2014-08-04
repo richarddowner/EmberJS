472696c6c22dd46c033cd3dc666cd0419ba08ba7
Richard <richarddowna@gmail.com>
2014-08-04 16:36:30 +1200

    indicate all in a collection at a specific state

--- TodoMVC/js/controllers/todos_controller.js
+++ TodoMVC/js/controllers/todos_controller.js
@@ -27,6 +27,10 @@ Todos.TodosController = Ember.ArrayController.extend({
     }
   },
 
+  allAreDone: function(key, value) {
+    return !!this.get('length') && this.isEvery('isCompleted');
+  }.property('@each.isCompleted'),
+
   hasCompleted: function() {
     return this.get('completed') > 0;
   }.property('completed'),
@@ -34,7 +38,7 @@ Todos.TodosController = Ember.ArrayController.extend({
   completed: function() {
     return this.filterBy('isCompleted', true).get('length');
   }.property('@each.isCompleted'),
-  
+
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
